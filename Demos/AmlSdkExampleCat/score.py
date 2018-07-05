530bd3f2223a20a1089c0c3e22677d4fb3694f00
grecoe <grecoe@microsoft.com>
2018-07-05 12:52:00 -0400

    deployment working

--- Demos/AmlSdkExampleCat/score.py
+++ Demos/AmlSdkExampleCat/score.py
@@ -1,8 +1,9 @@
 import os
 import pickle
 import json
+import pandas as pd
 from sklearn.externals import joblib
-from azureml.assets.persistence.persistence import get_model_path
+from azureml.core.model import Model
 
 # Prepare the web service definition by authoring
 # init() and run() functions. Test the functions
@@ -10,8 +11,7 @@ from azureml.assets.persistence.persistence import get_model_path
 def init():
     # load the model file
     global model
-    model_path = get_model_path(model_name = 'factorymodel.pkl')
-    #model_path = './factorymodel.pkl'
+    model_path = Model.get_model_path(model_name = 'factorymodel.pkl')
     print("Model Path Is: {}".format(model_path))
     model = joblib.load(model_path)
     print(model)
@@ -23,9 +23,9 @@ def run(input_df):
         # data = numpy.array(data)
         # result = model.predict(data)
         # return json.dumps({"result": result.tolist()})
-        print("Getting Prediction")
-        pred = model.predict(input_df)
-        print(str(pred))
+        data = json.loads(input_df)['input_df']
+        data = pd.DataFrame(data)
+        pred = model.predict(data)
         return json.dumps(str(pred))
     except Exception as e:
         result = str(e)
@@ -33,6 +33,11 @@ def run(input_df):
 
 def main():
     init()
+    
+    test_sample = json.dumps({'input_df':[{'id':1.0,'volt':241.0,'rotate':120.0,'temp':189.0,'time':3.0}]})
+    result = run(test_sample)
+    print("result {}".format(result))
+
 
 if __name__ == "__main__":
     main()
