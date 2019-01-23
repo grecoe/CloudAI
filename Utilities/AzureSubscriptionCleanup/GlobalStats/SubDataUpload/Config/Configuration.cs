ae102231e5d0b3a8380a9dfc5bb9f0a9bd044a1e
grecoe <grecoe@microsoft.com>
2019-01-23 15:04:09 -0500

    Upload application

---
+++ Utilities/AzureSubscriptionCleanup/GlobalStats/SubDataUpload/Config/Configuration.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using SubDataUpload.DataFormat;
+
+namespace SubDataUpload.Config
+{
+    class Configuration
+    {
+        #region Non-JSON Related
+        [JsonIgnore]
+        private const String CONFIG_FILE = "Configuration.json";
+        [JsonIgnore]
+        private const String DATA_FILE = "subscription_overview.json";
+        #endregion
+
+        [JsonProperty(PropertyName = "storageAccountName")]
+        public String StorageAccountName { get; set; }
+
+        [JsonProperty(PropertyName = "storageAccountKey")]
+        public String StorageAccountKey { get; set; }
+
+        [JsonProperty(PropertyName = "sourceDirectory")]
+        public String SourceDirectory { get; set; }
+
+        [JsonProperty(PropertyName = "latestDataContainer")]
+        public String LatestDataContainer { get; set; }
+
+        
+        protected Configuration()
+        {
+
+        }
+
+        public static Configuration LoadConfiguration()
+        {
+            String path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, CONFIG_FILE);
+            String content = System.IO.File.ReadAllText(path);
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<Configuration>(content);
+        }
+
+        public List<SubscriptionData> CollectSubscriptionInformation()
+        {
+            List<SubscriptionData> returnData = new List<SubscriptionData>();
+            if(System.IO.Directory.Exists(this.SourceDirectory) == false)
+            {
+                throw new Exception("Source directory is invalid");
+            }
+
+            foreach(String dir in System.IO.Directory.GetDirectories(this.SourceDirectory))
+            {
+                foreach(String file in System.IO.Directory.GetFiles(dir))
+                {
+                    if(String.Compare(System.IO.Path.GetFileName(file), DATA_FILE, true) == 0)
+                    {
+                        SubscriptionData data = Newtonsoft.Json.JsonConvert.DeserializeObject<SubscriptionData>(System.IO.File.ReadAllText(file));
+                        data.GroupData.Regions = this.SortDictionary(data.GroupData.Regions);
+                        data.Resources = this.SortDictionary(data.Resources);
+                        data.SubscriptionId = System.IO.Path.GetFileName(dir);
+
+                        returnData.Add(data);
+                    }
+                }
+            }
+
+            return returnData;
+        }
+
+        private Dictionary<String,int> SortDictionary(Dictionary<string,int> original)
+        {
+            // Create list of KeyValuePairs and sort on value
+            var myList = original.ToList();
+            myList.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
+
+            return myList.OrderByDescending(key => key.Value).ToDictionary(x => x.Key, x => x.Value);
+        }
+    }
+}
---
+++ Utilities/AzureSubscriptionCleanup/GlobalStats/SubDataUpload/DataFormat/SubscriptionData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace SubDataUpload.DataFormat
+{
+    class VirtualMachineData
+    {
+        public int Stopped { get; set; }
+        public int Running { get; set; }
+        public int Deallocated { get; set; }
+        public int Total { get; set; }
+    }
+
+    class ResourceGroupData
+    {
+        public int Total { get; set; }
+        public int Older60Days { get; set; }
+        public int Specials { get; set; }
+        public Dictionary<String, int> Regions { get; set; }
+
+        public ResourceGroupData()
+        {
+            this.Regions = new Dictionary<String, int>();
+        }
+    }
+
+
+
+    class SubscriptionData
+    {
+        [JsonIgnore]
+        public String SubscriptionId { get; set; }
+        [JsonProperty(PropertyName = "ResourceGroups")]
+        public ResourceGroupData GroupData { get; set; }
+        [JsonProperty(PropertyName = "VirtualMachines")]
+        public VirtualMachineData VirtualMachines { get; set; }
+        [JsonProperty(PropertyName = "ResourceUsage")]
+        public Dictionary<String,int> Resources { get; set; }
+
+        public SubscriptionData()
+        {
+            this.GroupData = new ResourceGroupData();
+            this.VirtualMachines = new VirtualMachineData();
+            this.Resources = new Dictionary<string, int>();
+        }
+    }
+}
